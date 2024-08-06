c34edae06072b12859b5c3beb0cac576229a416a
Eduardo <44793158+Chronickle25@users.noreply.github.com>
2024-08-06 16:50:33 -0400

    Update
    Se puede cambiar formato añadiendo la ruta específica.

--- main.py
+++ main.py
@@ -2,28 +2,45 @@ import os
 from PIL import Image
 import pillow_heif
 
-# set the directory path containing the HEIC files
-directory = 'heic_img'
+# Solicita la ruta del directorio que contiene las imágenes HEIC
+directory = input("Introduce la ruta del directorio que contiene las imágenes HEIC: ").strip()
 
-# loop through all files in the directory
+# Verifica que el directorio exista
+if not os.path.isdir(directory):
+    print("El directorio especificado no existe.")
+    exit()
+
+png_directory = os.path.join(directory, 'png_images')
+
+# Crea el directorio para las imágenes PNG si no existe
+if not os.path.exists(png_directory):
+    os.makedirs(png_directory)
+
+# Recorre todos los archivos en el directorio
 for filename in os.listdir(directory):
-     # check if the file is in HEIC format
-    if filename.endswith(".HEIC"):
-        # create an Image object from the HEIC file
+    # Verifica si el archivo está en formato HEIC
+    if filename.lower().endswith(".heic"):
+        # Crea la ruta completa del archivo
         filepath = os.path.join(directory, filename)
-        print("Converting:", filepath)
-        heif_file = pillow_heif.read_heif(filepath)
-        image = Image.frombytes(
-            heif_file.mode,
-            heif_file.size,
-            heif_file.data,
-            "raw",
-        )
+        print("Convirtiendo:", filepath)
+        
+        try:
+            # Lee el archivo HEIC
+            heif_file = pillow_heif.read_heif(filepath)
+            image = Image.frombytes(
+                heif_file.mode,
+                heif_file.size,
+                heif_file.data,
+                "raw",
+            )
 
-        # create a new filename for the PNG file
-        new_filename = os.path.splitext(filename)[0] + ".png"
-        new_filepath = os.path.join(directory, new_filename)
+            # Crea un nuevo nombre para el archivo PNG
+            new_filename = os.path.splitext(filename)[0] + ".png"
+            new_filepath = os.path.join(png_directory, new_filename)
 
-        image.save(new_filepath, format("png"))
+            # Guarda la imagen como PNG
+            image.save(new_filepath, format="png")
+            print(f"Imagen guardada como: {new_filepath}")
 
-
+        except Exception as e:
+            print(f"Error al convertir {filename}: {e}")
